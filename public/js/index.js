ab5f4ca9d4c964d2e1f491afab8c4e23f6d63a1e
ps1437 <praveen369soni@gmail.com>
2020-10-09 15:14:44 +0530

    hello

---
+++ public/js/index.js
@@ -0,0 +1,80 @@
+var $messages = $('.messages-content');
+
+
+
+$(window).load(function () {
+  $messages.mCustomScrollbar();
+  setTimeout(function () {
+    serverMessage("hello i am customer support bot type hi and i will show you quick buttions");
+  }, 100);
+
+});
+
+function updateScrollbar() {
+  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+    scrollInertia: 10,
+    timeout: 0
+  });
+}
+
+
+
+function insertMessage() {
+  msg = $('.message-input').val();
+  if ($.trim(msg) == '') {
+    return false;
+  }
+  $('<div class="message message-personal">' + msg + '</div> ').appendTo($('.mCSB_container')).addClass('new');
+  fetchmsg()
+
+  $('.message-input').val(null);
+  updateScrollbar();
+
+}
+
+document.getElementById("mymsg").onsubmit = (e) => {
+  e.preventDefault()
+  insertMessage();
+}
+
+function serverMessage(serverResponse) {
+
+
+  if ($('.message-input').val() != '') {
+    return false;
+  }
+  $('<div class="message loading new"><figure class="avatar"><i class="fas fa-robot bot-img"></i></figure><span></span></div>').appendTo($('.mCSB_container'));
+  updateScrollbar();
+
+
+  setTimeout(function () {
+    $('.message.loading').remove();
+    $('<div class="message new"><figure class="avatar"><i class="fas fa-robot bot-img"></i></figure>' + serverResponse + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    updateScrollbar();
+  }, 100 + (Math.random() * 20) * 100);
+
+}
+
+
+function fetchmsg() {
+
+  var url = '/api';
+
+  const data = new URLSearchParams();
+
+  for (const pair of new FormData(document.getElementById("mymsg"))) {
+    data.append(pair[0], pair[1]);
+  }
+
+  fetch(url, {
+    method: 'POST',
+    body: data
+  }).then(res => res.json())
+    .then(response => {
+      serverMessage(response.bot);
+    })
+    .catch(error => console.error('Error h:', error));
+
+}
+
+
---
+++ server.js
@@ -0,0 +1,68 @@
+const dialogflow = require('@google-cloud/dialogflow');
+const uuid = require('uuid');
+var express = require('express');
+var bodyparser = require('body-parser');
+var cors = require('cors')
+var app = express();
+
+const PROJECT_ID = 'chatbot-nfey'; //https://dialogflow.com/docs/agents#settings
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+const sessionId = uuid.v4();
+const PORT = process.env.port | 8081;
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/api', (req, res) => {
+  runSample(req.body.message).then(data => {
+    console.log("----------"+data);
+    res.send({ "bot": data });
+  })
+});
+
+async function runSample(query, projectId = PROJECT_ID) {
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+    keyFilename: "chatbot-nfey-2722497a4fe0.json"
+  });
+  const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: query,
+        // The language used by the client (en-US)
+        languageCode: 'en-US',
+      },
+    },
+  };
+  const res =  await sessionClient.detectIntent(request).then(responses => {
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+      console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+      console.log(`  No intent matched.`);
+    }
+    return result.fulfillmentText;
+  });
+return res;
+}
+
+app.listen(PORT, function () {
+  console.log(`Server running at port ${PORT}`);
+})
+
+runSample("good morning").then(res => console.log(res));
